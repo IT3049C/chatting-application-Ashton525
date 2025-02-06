a9d2ebde5b1594b63a58af76e6f3e2bd29ac6f60
Ashton525 <ashtoncashier@gmail.com>
2025-02-06 15:07:04 -0500

    Started Project. Finished HTML. Working on javascript format function

---
+++ resources/js/index.js
@@ -0,0 +1,9 @@
+const nameInput = document.getElementById('my-name-input')
+const messageInput = document.getElementById('my-message-input')
+const sendButton = document.getElementById('send-button')
+const chatBox = document.getElementById('chat')
+
+function formatchat (message, userNameInput){
+  const time = new Date().message.timestamp
+  const readabletime = `${(time.gethours())} + : + ${time.getMinutes()}`
+}
