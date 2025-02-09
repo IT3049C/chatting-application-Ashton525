2f9246d28a7b546125890dd23d507f2acdc578d7
Ashton525 <ashtoncashier@gmail.com>
2025-02-09 15:18:20 -0500

    Step 2 Function Complete

--- resources/js/index.js
+++ resources/js/index.js
@@ -6,4 +6,34 @@ const chatBox = document.getElementById('chat')
 function formatchat (message, userNameInput){
   const time = new Date().message.timestamp
   const readabletime = `${(time.gethours())} + : + ${time.getMinutes()}`
+
+  if (userNameInput === message.sender)
+  {
+    return `
+    <div class="mine messages">
+    <div class="message">
+      ${message.text}
+      </div>
+      <div class = "sender-info">
+      ${readabletime}
+      </div>
+      </div>
+    `
+    //must use `when translating to html
+  }
+  else {
+    return `
+    <div class = "yours messages">
+    <div class = "message">
+    ${message.text}
+    </div>
+    <div class = "sender-info">
+    ${message.sender} ${readabletime}
+    </div>
+    `
+  }
+}
+
+function fetchMessages(){
+  return fetch(`messages`)
 }
